268ac71ac149abd31660d4223a021d90a1976053
Laura-Neff <ljneff@gmail.com>
2021-02-06 15:57:41 -0500

    Implemented allocation functions for PBM and PGM

--- pbm_aux.c
+++ pbm_aux.c
@@ -1,7 +1,12 @@
 #include "pbm.h"
 #include <stdio.h> //error library
+#include <stdlib.h>
 
-
+/*int main( int argc, char *argv[] )
+{
+    new_pbmimage(10,11);
+}
+*/
 
 PPMImage * new_ppmimage( unsigned int w, unsigned int h, unsigned int m )
 {
@@ -16,25 +21,7 @@ PPMImage * new_ppmimage( unsigned int w, unsigned int h, unsigned int m )
 
 PBMImage * new_pbmimage( unsigned int w, unsigned int h )
 {
-
-    //allocate for null characters when it comes to strings
-
-    int *my2DArray = malloc(w * h * sizeof(unsigned int));
-                     //allocate w * h * 4 bytes (because of int) of memory
-
-    if (*my2DArray == 0) //See if we have enough memory to grab from heap after program running
-    {
-        fprintf(stderr, "Error: Cannot allocate memory.\n");
-        exit(1);
-    }
-
-
-
-
-
-
-
-
+    //______________________________________________________________________________-//
 
     //1) define PBMImage pointer
     //2) allocate storage for PBMImage struct for PBMImage pointer
@@ -46,21 +33,59 @@ PBMImage * new_pbmimage( unsigned int w, unsigned int h )
         //look at malloc example
         //pixmap should point to an array of pointers, one pointer for each row pixmap
         //each pixmap row pointer should point to an array of unsigned integers, one unsigned integer for each column
-
     //5)return pointer to PBMImage struct
+    //______________________________________________________________________________-//
 
 
-
-    //Read PPM file
+    //*****Side Notes******//
+        //Read PPM file
         //look at pixel values of the PPM and decide whether each pixel should be a 0 or a 1
 
-    //𝐴𝑣𝑒𝑟𝑎𝑔𝑒(𝑅+𝐺+𝐵)<𝑃𝑃𝑀𝑀𝑎𝑥/2 
+        //𝐴𝑣𝑒𝑟𝑎𝑔𝑒(𝑅+𝐺+𝐵)<𝑃𝑃𝑀𝑀𝑎𝑥/2 
         //If this is < 2, output 0
         //If this is > 2, output 1
+    //*****Side Notes******//
+
+
+
+    /*
+        if (*my2DArray == 0) //See if we have enough memory to grab from heap after program running
+        {
+            fprintf(stderr, "Error: Cannot allocate memory.\n");
+            exit(1);
+        } */
+
+     //PBMImage * myNewImage = malloc(sizeof(PBMImage)); //tell program that we are declaring a variable to talk about an address in memory.
+                                                        //Then tell program to allocate enough memory from the heap for a PBMImage struct
+                                                        //We use pointers when talking about malloc
+                                                
+
+    PBMImage *myNewImage = malloc(sizeof(PBMImage)); //PBM pointer defined and storage allocated. Step 1 and 2 completed
+    myNewImage->height = h;  //height initialized.  //-> is the same as the dot operator for a pointer variable   
+    myNewImage->width = w;   //width initialized. Step 3 completed
+    //printf("%d", myNewImage->height);
+
+    //int *my2DArray = malloc(w * h * sizeof(unsigned int));
+                     //allocate w * h * 4 bytes (because of int) of memory
+
+    myNewImage->pixmap = malloc(w * h * sizeof(unsigned int));  //pixmap allocated. Step 4 complete
+
+    return(myNewImage); //Pointer returned. Step 5 complete
+
 }
 
 PGMImage * new_pgmimage( unsigned int w, unsigned int h, unsigned int m )
 {
+    PGMImage *myNewImage = malloc(sizeof(PGMImage)); //PGM pointer defined and storage allocated. Step 1 and 2 completed
+    myNewImage->height = h;  //height initialized.  //-> is the same as the dot operator for a pointer variable   
+    myNewImage->width = w;   //width initialized. Step 3 completed
+
+    //int *my2DArray = malloc(w * h * sizeof(unsigned int));
+                     //allocate w * h * 4 bytes (because of int) of memory
+
+    myNewImage->pixmap = malloc(w * h * sizeof(unsigned int));  //pixmap allocated. Step 4 complete
+
+    return(myNewImage); //Pointer returned. Step 5 complete
 }
 
 void del_ppmimage( PPMImage * p )
@@ -74,7 +99,7 @@ void del_pgmimage( PGMImage * p )
 
 void del_pbmimage( PBMImage * p )
 {
-    free(p);
+    //free(p);
     //add more stuff here
 }
 
